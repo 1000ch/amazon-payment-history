a40e8158ac05453af36c0d1af0c3e3250e724f96
1000ch <shogo.sensui@gmail.com>
2016-08-19 12:33:07 +0900

    Fix

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,27 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
+  chrome.tabs.getAllInWindow(undefined, tabs => {
+    for (const tab of tabs) {
+      if (tab.url && tab.url.includes('https://www.amazon.co.jp/gp/css/order-history')) {
+        chrome.tabs.update(tab.id, {selected: true});
+        return;
+      }
+    }
+
+    chrome.tabs.create({url: 'https://www.amazon.co.jp/gp/css/order-history'});
+  });
+
   chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-    const dl = document.querySelector('dl');
+    const container = document.querySelector('.Container');
     let all = 0;
     Object.keys(data).forEach(year => {
-      const dt = document.createElement('dt');
-      const dd = document.createElement('dd');
+      let item = document.createElement('div');
+      let y = document.createElement('div');
+      let p = document.createElement('div');
 
       let total = 0;
-      data[year].forEach(item => total += item.price);
+      data[year].forEach(i => total += i.price);
       all += total;
 
-      dt.textContent = `${year}年`;
-      dd.textContent = `${total}円`;
-      dl.appendChild(dt);
-      dl.appendChild(dd);
+      item.className = 'Item';
+      y.textContent = `${year}年`;
+      y.className = 'Item__Year';
+      p.textContent = `${total}円`;
+      p.className = 'Item__Price';
+
+      item.appendChild(y);
+      item.appendChild(p);
+      container.appendChild(item);
     });
 
-    const dt = document.createElement('dt');
-    const dd = document.createElement('dd');
-    dt.textContent = '合計';
-    dd.textContent = `${all}円`;
-    dl.appendChild(dt);
-    dl.appendChild(dd);
+    const item = document.createElement('div');
+    const t = document.createElement('div');
+    const y = document.createElement('div');
+    item.className = 'Item';
+    t.textContent = '合計';
+    t.className = 'Item__Year';
+    y.textContent = `${all}円`;
+    y.className = 'Item__Price';
+    item.appendChild(t);
+    item.appendChild(y);
+    container.appendChild(item);
   });
 
   chrome.tabs.executeScript({
