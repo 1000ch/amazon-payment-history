bd088abe02b234bf136001d788e6e3c9dedf8c35
1000ch <shogo.sensui@gmail.com>
2016-08-19 16:55:32 +0900

    Fix popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,20 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+  function executeScript() {
+    chrome.tabs.executeScript({
+      file: 'js/jquery-3.1.0.min.js'
+    }, () => {
+      chrome.tabs.executeScript({
+        file: 'js/script.js'
+      });
+    });
+  }
+
   chrome.tabs.getAllInWindow(undefined, tabs => {
     for (const tab of tabs) {
       if (tab.url && tab.url.includes('https://www.amazon.co.jp/gp/css/order-history')) {
-        chrome.tabs.update(tab.id, {selected: true});
+        chrome.tabs.update(tab.id, {
+          active: true
+        }, tab => executeScript());
         return;
       }
     }
 
-    chrome.tabs.create({url: 'https://www.amazon.co.jp/gp/css/order-history'});
+    chrome.tabs.create({
+      url: 'https://www.amazon.co.jp/gp/css/order-history',
+      active: true
+    }, tab => executeScript());
   });
 
   chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-    const loading = document.querySelector('.Loading');
+    document.querySelector('.Loading').classList.add('-hidden');
     const container = document.querySelector('.Container');
-
-    loading.classList.add('-hidden');
 
     let all = 0;
     Object.keys(data).forEach(year => {
@@ -32,13 +45,5 @@ document.addEventListener('DOMContentLoaded', () => {
     div.className = 'Item';
     div.textContent = `合計: ${all}円`;
     container.appendChild(div);
-  });
-
-  chrome.tabs.executeScript({
-    file: 'js/jquery-3.1.0.min.js'
-  }, () => {
-    chrome.tabs.executeScript({
-      file: 'js/script.js'
-    });
   });
 });
