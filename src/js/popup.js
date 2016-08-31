0a63a85a1400cc9a90646f1f430e1fccc98516de
1000ch <shogo.sensui@gmail.com>
2016-08-31 22:26:17 +0900

    Calc average more

--- src/js/popup.js
+++ src/js/popup.js
@@ -25,7 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     div = document.createElement('div');
     div.className = 'Item';
-    div.textContent = `平均: ${Math.round(all / results.length).toLocaleString()}円/年`;
+    div.textContent = `年平均: ${Math.round(all / results.length).toLocaleString()}円`;
+    container.appendChild(div);
+
+    div = document.createElement('div');
+    div.className = 'Item';
+    div.textContent = `月平均: ${Math.round(all / (results.length * 12)).toLocaleString()}円`;
     container.appendChild(div);
 
     const url = 'https://chrome.google.com/webstore/detail/pgccjlmicdpgkbllgiafapgbnciodipb';
